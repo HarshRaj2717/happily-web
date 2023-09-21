f1c405a6549966796be182d62cebb244389bc709
harshraj2717 <harshraj2717@gmail.com>
2023-09-21 23:40:47 +0530

    fix infinite api calls

--- app/scales/[scale_link]/result/page.js
+++ app/scales/[scale_link]/result/page.js
@@ -1,10 +1,10 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { getResult } from "./getResult";
 import { Paper, Avatar, Grid } from "@mui/material";
-import Typography from '@mui/joy/Typography';
-import './Result.css'
+import Typography from "@mui/joy/Typography";
+import "./Result.css";
 
 export default function SearchBar() {
   const searchParams = useSearchParams();
@@ -12,38 +12,38 @@ export default function SearchBar() {
   const user_res = searchParams.get("user_res");
   const [result, setResult] = useState("");
 
-  const resultGetter = async () => {
-    const api_data = await getResult(scale_link, user_res);
-    if (api_data.success !== 1) return;
-    setResult(api_data);
-  };
+  useEffect(() => {
+    const resultGetter = async () => {
+      const api_data = await getResult(scale_link, user_res);
+      if (api_data.success !== 1) return;
+      setResult(api_data);
+    };
 
-  resultGetter();
+    resultGetter();
+  }, []);
 
   if (result === "") {
     return <></>;
   }
 
   if (scale_link == "gidyq-aa-male" || scale_link == "gidyq-aa-female") {
-    return <>
-
-
-
-
-    </>;
+    return <></>;
   } else if (scale_link == "dass-y") {
-    return <>
-    <div className="bb">
-      <div className="container text-center p-3">
-      <Typography className="m-auto" level="h2" color="primary">
-      Your Results
-    </Typography>
-    <div className="m-auto text-gray-600 text-sm">{scale_link}</div>
-    <div className="m-auto text-black text-lg font">{scale_link.result.total}</div>
-      </div>
-      </div>
-     
-    </>;
+    return (
+      <>
+        <div className="bb">
+          <div className="container text-center p-3">
+            <Typography className="m-auto" level="h2" color="primary">
+              Your Results
+            </Typography>
+            <div className="m-auto text-gray-600 text-sm">{scale_link}</div>
+            <div className="m-auto text-black text-lg font">
+              {result.result.total}
+            </div>
+          </div>
+        </div>
+      </>
+    );
   } else {
     return <></>;
   }
