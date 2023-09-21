5f0ebcfc29e839b3f77e7372ef8a250be1228c46
TheAshutoshGupta <ashutoshg479@gmail.com>
2023-09-21 23:39:22 +0530

    comfig

--- app/scales/[scale_link]/result/page.js
+++ app/scales/[scale_link]/result/page.js
@@ -2,6 +2,9 @@
 import { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { getResult } from "./getResult";
+import { Paper, Avatar, Grid } from "@mui/material";
+import Typography from '@mui/joy/Typography';
+import './Result.css'
 
 export default function SearchBar() {
   const searchParams = useSearchParams();
@@ -22,9 +25,25 @@ export default function SearchBar() {
   }
 
   if (scale_link == "gidyq-aa-male" || scale_link == "gidyq-aa-female") {
-    return <></>;
+    return <>
+
+
+
+
+    </>;
   } else if (scale_link == "dass-y") {
-    return <></>;
+    return <>
+    <div className="bb">
+      <div className="container text-center p-3">
+      <Typography className="m-auto" level="h2" color="primary">
+      Your Results
+    </Typography>
+    <div className="m-auto text-gray-600 text-sm">{scale_link}</div>
+    <div className="m-auto text-black text-lg font">{scale_link.result.total}</div>
+      </div>
+      </div>
+     
+    </>;
   } else {
     return <></>;
   }
